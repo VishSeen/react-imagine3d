c42d589e7836f68bccbbf1331cd3ee2cd4fbeb53
vishseen <vishseenarain@gmail.com>
2024-01-29 03:29:52 +0400

    [DEV] - nav bar menu started. changed website name in manifest.

--- src/App.js
+++ src/App.js
@@ -2,20 +2,36 @@ import React from "react";
 import "./styles/global.css";
 import PROJECT_PAGE_QUERY from './gql-query/ProjectPageQuery'
 import { useQuery } from '@apollo/client';
-
+import config from './config.json'
+import logo from './assets/logo/white.png'
+import './styles/test.css'
 
 
 function App() {
-    const { loading, error, data } = useQuery(PROJECT_PAGE_QUERY);
-    return (
-        <div className="App">
-            <header className="App-header">
-                <h1>Hello React</h1>
-            </header>
+	const { loading, error, data } = useQuery(PROJECT_PAGE_QUERY);
 
-				{loading ? <p>Loading...</p> : data?.projectPage.title}
-        </div>
-    );
+	return (
+		<div className="App">
+			<div className="top-bar">
+				<div className="logo">
+					<img src={logo} style={{ width: '125px' }} />
+				</div>
+
+				<p>
+					<span className={config.icons}>
+						menu
+					</span>
+				</p>
+			</div>
+
+			{loading ? <p>Loading...</p> : data?.projectPage.title}
+
+
+			<footer>
+				© 2023 Vishroy Seenarain. All rights reserved.
+			</footer>
+		</div>
+	);
 }
 
 export default App;
---
+++ src/components/nav-bar/nav-bar.js
@@ -0,0 +1,14 @@
+import StyledNav from "./style"
+
+
+const NavBar = () => {
+    return(
+        <StyledNav className="nav-bar">
+            <img />
+
+
+            <span>NAVIGATION</span>
+
+        </StyledNav>
+    )
+}
