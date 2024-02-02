0e915eb0de95c9cb2a30fb0d27eaf7538561c3d9
Divesh Heeramun <divesh.heeramun1@gmail.com>
2024-02-02 21:55:05 +0400

    Add basic projects page and routes

--- src/App.js
+++ src/App.js
@@ -1,33 +1,54 @@
-import React, { useState } from "react";
-import "./styles/global.css";
-import PROJECT_PAGE_QUERY from './gql-query/ProjectPageQuery'
 import { useQuery } from '@apollo/client';
-import config from './config.json'
-import logo from './assets/logo/white.png'
-import './styles/test.css'
-import NavBar from "./components/nav-bar/nav-bar";
-import TopBar from "./components/top-bar/top-bar";
+import React, { useState } from 'react';
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 
+import logo from './assets/logo/white.png';
+import NavBar from './components/nav-bar/nav-bar';
+import TopBar from './components/top-bar/top-bar';
+import config from './config.json';
+
+import PROJECT_PAGE_QUERY from './gql-query/ProjectPageQuery';
+
+import './styles/global.css';
+import './styles/test.css';
+
+import HomePage from './pages/homepage/homepage';
+import Projects from './pages/projects';
 
 function App() {
-	const { loading, error, data } = useQuery(PROJECT_PAGE_QUERY);
-	const [menuOpen, setMenuOpen] = useState(false);
-    const menuBtnClick = () => setMenuOpen(!menuOpen);
+  const { loading, error, data } = useQuery(PROJECT_PAGE_QUERY);
+  const [menuOpen, setMenuOpen] = useState(false);
+  const menuBtnClick = () => setMenuOpen(!menuOpen);
 
-	return (
-		<div className="App">
-            <TopBar isOpened={menuOpen} onOpenMenu={menuBtnClick} />
+  return (
+    <div className="App">
+      <TopBar
+        isOpened={menuOpen}
+        onOpenMenu={menuBtnClick}
+      />
 
-			<NavBar isOpened={menuOpen} />
+      <NavBar isOpened={menuOpen} />
 
-			{/* {loading ? <p>Loading...</p> : data?.projectPage.title} */}
+      <Router>
+        <Routes>
+          <Route
+            path="/"
+            element={<HomePage />}
+          />
+          <Route
+            path="/projects"
+            element={<Projects />}
+          />
+        </Routes>
+      </Router>
 
+      {/* {loading ? <p>Loading...</p> : data?.projectPage.title} */}
 
-			{/* <footer>
+      {/* <footer>
 				© 2023 Vishroy Seenarain. All rights reserved.
 			</footer> */}
-		</div>
-	);
+    </div>
+  );
 }
 
 export default App;
