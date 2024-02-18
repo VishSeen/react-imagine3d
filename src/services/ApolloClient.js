a3298b478649dbe5f7227c1a5545495e871c9dac
Divesh Heeramun <divesh.heeramun1@gmail.com>
2024-02-18 19:33:39 +0400

    Move out Contentful credentials from codebase (#1)

--- src/services/ApolloClient.js
+++ src/services/ApolloClient.js
@@ -1,11 +1,14 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client';
 
+const contentfulSpaceId = process.env.REACT_APP_CONTENTFUL_SPACE_ID;
+const contentfulBearerToken = process.env.REACT_APP_CONTENTFUL_BEARER_TOKEN;
+
 const Client = new ApolloClient({
-    uri: `https://graphql.contentful.com/content/v1/spaces/obr0lam7iwdw/environments/master`,
-    cache: new InMemoryCache(),
-    headers: {
-        authorization: `Bearer 93urIsWeJNXPmSp3-PrRWSWZmSVloKDbJ98SDVY7SCg`
-    }
+  uri: `https://graphql.contentful.com/content/v1/spaces/${contentfulSpaceId}/environments/master`,
+  cache: new InMemoryCache(),
+  headers: {
+    authorization: `Bearer ${contentfulBearerToken}`
+  }
 });
 
 export default Client;
